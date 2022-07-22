e262e52b4e23eeb4c118249246c549cd9ccf0915
ThinhLe881 <thinhleminh201@gmail.com>
2022-07-21 23:15:48 -0400

    Complete file manager

--- file_manager.py
+++ file_manager.py
@@ -1,13 +1,21 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
 
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 src_dir = ''
 dest_dir_music = ''
 dest_dir_video = ''
 dest_dir_image = ''
-dest_dir_doc = ''
+dest_dir_documents = ''
 dest_dir_compressed = ''
-dest_dir_other = ''
+dest_dir_program = ''
 
 # supported image types
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
@@ -23,3 +31,89 @@ document_extensions = [".doc", ".docx", ".odt",
                        ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 # supported Compressed types
 compressed_extensions = [".rar", ".arj", ".tar.gz", ".tgz", ".gz", ".iso", ".7z", ".zip", ".zipx", ".z"]
+
+
+def make_unique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    # if file already exists, add a number to the end of the file name
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+    return name
+
+
+def move_file(dest, entry, name):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(dest, name)
+        oldName = join(dest, name)
+        newName = join(dest, unique_name)
+        rename(oldName, newName)
+    move(entry, dest)
+
+
+class MoverHandler(FileSystemEventHandler):
+    # this function is called when there is a change in the source directory
+    # .upper is for not missing out on files with uppercase extensions
+    def on_modified(self, event):
+        with scandir(src_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+                self.check_compressed_files(entry, name)
+                self.check_program_files(entry, name)
+
+    def check_audio_files(self, entry, name):  # checks all Audio files
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()): 
+                move_file(dest_dir_music, entry, name)
+                logging.info(f"Moved audio file: {name}")
+
+    def check_video_files(self, entry, name):  # checks all Video files
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move_file(dest_dir_video, entry, name)
+                logging.info(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):  # checks all Image files
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move_file(dest_dir_image, entry, name)
+                logging.info(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):  # checks all Document files
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move_file(dest_dir_documents, entry, name)
+                logging.info(f"Moved document file: {name}")
+    
+    def check_compressed_files(self, entry, name):  # checks all Compressed files
+        for compressed_extension in compressed_extensions:
+            if name.endswith(compressed_extension) or name.endswith(compressed_extension.upper()):
+                move_file(dest_dir_compressed, entry, name)
+                logging.info(f"Moved compressed file: {name}")
+
+    def check_program_files(self, entry, name):  # checks all Program files
+        if name.endswith(".exe") or name.endswith(".EXE"):
+            move_file(dest_dir_program, entry, name)
+            logging.info(f"Moved program file: {name}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = src_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
