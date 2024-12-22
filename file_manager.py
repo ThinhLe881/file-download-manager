4692e7cd5f9461ff41a3867be286ad2b670473ad
ThinhLe881 <thinhleminh201@gmail.com>
2024-12-21 22:52:21 -0500

    remove unncessary code

--- file_manager.py
+++ file_manager.py
@@ -7,39 +7,22 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-def make_unique(dest, name):
-    filename, extension = splitext(name)
-    counter = 1
-    # if file already exists, add a number to the end of the file name
-    while exists(f"{dest}/{name}"):
-        name = f"{filename}({str(counter)}){extension}"
-        counter += 1
-    return name
-
-def move_file(dest, entry, name):
-    if exists(f"{dest}/{name}"):
-        unique_name = make_unique(dest, name)
-        oldName = join(dest, name)
-        newName = join(dest, unique_name)
-        rename(oldName, newName)
-    move(entry, dest)
-
 
 class MoverHandler(FileSystemEventHandler):
     # supported image types
-    image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", 
+    image_extensions = {".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", 
                         ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", 
                         ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", 
-                        ".svg", ".svgz", ".ai", ".eps", ".ico"]
+                        ".svg", ".svgz", ".ai", ".eps", ".ico"}
     # supported Video types
-    video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
-                        ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+    video_extensions = {".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                        ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"}
     # supported Audio types
-    audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+    audio_extensions = {".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"}
     # supported Document types
-    document_extensions = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+    document_extensions = {".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"}
     # supported Compressed types
-    compressed_extensions = [".rar", ".arj", ".tar.gz", ".tgz", ".gz", ".iso", ".7z", ".zip", ".zipx", ".z"]
+    compressed_extensions = {".rar", ".arj", ".tar.gz", ".tgz", ".gz", ".iso", ".7z", ".zip", ".zipx", ".z"}
 
     def __init__(self, src_dir, dest_dir_music, dest_dir_video, dest_dir_image, 
                     dest_dir_document, dest_dir_compressed, dest_dir_program):
@@ -56,49 +39,51 @@ class MoverHandler(FileSystemEventHandler):
     def on_modified(self, event):
         with scandir(self.src_dir) as entries:
             for entry in entries:
-                name = entry.name
-                print(name)
-                self.check_audio_files(entry, name)
-                self.check_video_files(entry, name)
-                self.check_image_files(entry, name)
-                self.check_document_files(entry, name)
-                self.check_compressed_files(entry, name)
-                self.check_program_files(entry, name)
+                self.f_entry = entry
+                self.f_name, self.f_extension = splitext(self.f_entry.name)
+                self.f_extension.lower()
 
-    def check_audio_files(self, entry, name):  # checks all Audio files
-        for audio_extension in self.audio_extensions:
-            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
-                move_file(self.dest_dir_music, entry, name)
-                logging.info(f"Moved audio file: {name}")
+                if self.f_extension in self.audio_extensions: # checks all Audio files
+                    self.move_file(self.dest_dir_music, entry, self.f_entry)
+                    logging.info(f"Moved audio file: {self.f_entry}")
+                    continue
+                if self.f_extension in self.video_extensions: # checks all Video files
+                    self.move_file(self.dest_dir_video, entry, self.f_entry)
+                    logging.info(f"Moved video file: {self.f_entry}")
+                    continue
+                if self.f_extension in self.image_extensions: # checks all Image files
+                    self.move_file(self.dest_dir_image, entry, self.f_entry)
+                    logging.info(f"Moved image file: {self.f_entry}")
+                    continue
+                if self.f_extension in self.document_extensions: # checks all Document files
+                    self.move_file(self.dest_dir_document, entry, self.f_entry)
+                    logging.info(f"Moved document file: {self.f_entry}")
+                    continue
+                if self.f_extension in self.compressed_extensions: # checks all Compressed files
+                    self.move_file(self.dest_dir_compressed, entry, self.f_entry)
+                    logging.info(f"Moved compressed file: {self.f_entry}")
+                    continue
+                if self.f_extension == ".exe": # checks all Program files
+                    self.move_file(self.dest_dir_program, entry, self.f_entry)
+                    logging.info(f"Moved program file: {self.f_entry}")
 
-    def check_video_files(self, entry, name):  # checks all Video files
-        for video_extension in self.video_extensions:
-            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
-                move_file(self.dest_dir_video, entry, name)
-                logging.info(f"Moved video file: {name}")
 
-    def check_image_files(self, entry, name):  # checks all Image files
-        for image_extension in self.image_extensions:
-            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
-                move_file(self.dest_dir_image, entry, name)
-                logging.info(f"Moved image file: {name}")
+    def make_unique(self, dest):
+        counter = 1
+        # if file already exists, add a number to the end of the file name
+        while exists(f"{dest}/{name}"):
+            name = f"{self.f_name}({str(counter)}){self.f_extension}"
+            counter += 1
+        return name
 
-    def check_document_files(self, entry, name):  # checks all Document files
-        for document_extension in self.document_extensions:
-            if name.endswith(document_extension) or name.endswith(document_extension.upper()):
-                move_file(self.dest_dir_document, entry, name)
-                logging.info(f"Moved document file: {name}")
-    
-    def check_compressed_files(self, entry, name):  # checks all Compressed files
-        for compressed_extension in self.compressed_extensions:
-            if name.endswith(compressed_extension) or name.endswith(compressed_extension.upper()):
-                move_file(self.dest_dir_compressed, entry, name)
-                logging.info(f"Moved compressed file: {name}")
 
-    def check_program_files(self, entry, name):  # checks all Program files
-        if name.endswith(".exe") or name.endswith(".EXE"):
-            move_file(self.dest_dir_program, entry, name)
-            logging.info(f"Moved program file: {name}")
+    def move_file(self, dest):
+        if exists(f"{dest}/{self.f_entry.name}"):
+            unique_name = self.make_unique(dest)
+            oldName = join(dest, self.f_entry.name)
+            newName = join(dest, unique_name)
+            rename(oldName, newName)
+        move(self.f_entry, dest)
 
 
 def main():
